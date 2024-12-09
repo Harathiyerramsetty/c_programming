311883d46e351abc8f5772eae5977ce9ec3e76e3
Harathiyerramsetty <harathiyharathi@gmail.com>
2024-12-09 15:17:58 +0530

    Create numbers.c

---
+++ numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i,n;
+    printf("enter the n values:");
+    scanf("%d",&n);
+    for(i=0;i<=n;++i)
+{
+    printf("%d\t",i);
+}    
+    return 0;
+}
