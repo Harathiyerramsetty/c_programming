12f839c5f94dad78eaf0a09bdf67969cc01f58cd
Harathiyerramsetty <harathiyharathi@gmail.com>
2024-12-23 14:53:03 +0530

    Create fibonacci.c

---
+++ fibonace.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    int n,i,a=0,b=1,c;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    printf("%d\n%d\n",a,b);
+    for(i=0;i<n;++i)
+    {
+        c=a+b;
+        printf("%d\n",c);
+        a=b;
+        b=c;
+    }
+    printf("Print the value of c is %d",c);
+return 0;
+}
